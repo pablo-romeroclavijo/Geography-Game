6102ebe5c52bfaa56c8fa75a644fd8686e7ddd9f
pablo-romeroclavijo <136720541+pablo-romeroclavijo@users.noreply.github.com>
2023-09-18 16:14:50 +0100

    updated flag_quiz

--- client/flag_quizz/flag_quiz.js
+++ client/flag_quizz/flag_quiz.js
@@ -1,14 +1,39 @@
 const query = document.querySelector("#level")
+const question = document.querySelector('#question')
+const options = document.querySelector('#options')
 
 query.addEventListener("submit", getCountries)
 
 //functions
 
-function createQuestion(e){
-  console.log('running')
-  e.preventDefault()
+function createQuestion(data){
   const countries = data
-  console.log(countries)
+  const randomIndex = Math.floor(Math.random()*countries.length)
+  const answer = countries[randomIndex]
+  console.log(answer)
+
+  for(country in countries){
+    let ID = countries[country].ID
+    let type = 'flags'
+    console.log(ID, type)
+    fetchImage(type, ID)
+  }
+
+}
+
+function addImage(url){
+  let input = document.createElement('input')
+  input.type = 'radio'
+  options.appendChild(input)
+
+  let label = document.createElement('label')
+  label.setAttribute('for', "input")
+  options.appendChild(label)
+
+  let img = document.createElement('img')
+  img.src = url
+  label.appendChild(img)
+
 }
 
 
@@ -28,7 +53,7 @@ async function fetchCountries(level, region, numberRequests) {
     const response = await fetch(`http://localhost:3000/countries/${level}&${region}&${numberRequests}`);
     if(response.ok){
         const data = await  response.json()
-        console.log(data)
+        createQuestion(data)
     }
     else {throw 'Error status: ' + resp.status}
   }
@@ -36,3 +61,18 @@ async function fetchCountries(level, region, numberRequests) {
 }
 
 
+async function fetchImage(type, ID, country) {
+  
+  //Make sure to add your deployed API URL in this fetch
+  // try {
+    const response = await fetch(`http://localhost:3000/image/${type}/${ID}`);
+    if(response.ok){
+      const imageBlob = await response.blob()
+      const imageURL = URL.createObjectURL(imageBlob)
+      console.log(imageURL)
+      addImage(imageURL)
+    }
+    else {throw 'Error status: ' + res.status}
+  }
+  // catch(e){console.log('error at  catch')}
+// }
