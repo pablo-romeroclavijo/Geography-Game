9e7836d61abd049c89a03017b63ce5b302cf8110
khalifa <oriyomibamikole@gmail.com>
2023-09-18 13:01:20 +0100

    9 to 10 for top score

--- server/app.js
+++ server/app.js
@@ -65,7 +65,7 @@ app.post('/updateScore', (req, res) =>{
 
 app.get('/scoreBoard', (req, res) => {
     const sortedScoreBoard = scoreBoard.sort((a, b) => b.score - a.score)
-    const topScore = sortedScoreBoard.slice(0, 9)
+    const topScore = sortedScoreBoard.slice(0, 10)
     console.log(topScore)
     res.status(200).send(JSON.stringify(topScore))
 })
