63ca9bb6c0536be21566ea7ca2d7b38dfcdb0e69
mgr-mirnal <mgr.mrinal97@gmail.com>
2023-09-19 01:59:37 +0100

    managed to update options and image with the api data

--- client/quiz/quiz.js
+++ client/quiz/quiz.js
@@ -1,33 +1,58 @@
 
-const questions = document.querySelector("#questions")
+const question = document.querySelector("#questions")
 const setting = document.querySelector("#setting")
-
+const submit = document.querySelector("#answerSubmit")
+const worldMap = document.querySelector("#worldMap")
+const optionSelect = document.querySelector("#options")
 
 setting.addEventListener("submit",getCountries)
 
 let id
 let countries = {}
 
-hbk
+
 function startQuiz(q){
+  const randomIndex = Math.floor(Math.random()*q.length)
+  console.log(randomIndex)
+  let id;
+  id = q[randomIndex].ID
+  console.log(id)
+  fetchImage("maps",id)
   q.forEach(element => {
     console.log(element)
-    countries[element.name] = element.name
-    id = element.ID
-  });
-  console.log(id)
-  console.log(countries)
-  showmap(id)
+    generateOption(element.name,element.ID)
+  })
+}
+function generateOption(name,id){
+  console.log(name)
+  let option1 = document.createElement("input")
+  option1.type = "radio"
+  option1.id = id
+  option1.name = name
+  console.log(option1)
+  optionSelect.appendChild(option1)
+ 
+  let label = document.createElement('label')
+  console.log(option1)
+  label.setAttribute('for', option1.id)
+  label.textContent = name
+  optionSelect.appendChild(label)
 }
 
-function showmap(id){
-  const image = document.createElement("img")
-  image.src = `../../server/assets/Maps/m${id}.png`
-  image.width = 1500
-  questions.appendChild(image)
+function addImage(url){
+  let map = document.createElement("img")
+  map.src = url
+  map.width = 1500
+  worldMap.appendChild(map)
 }
+
 function getCountries(e){
   e.preventDefault()
+  worldMap.innerHTML = ""
+  optionSelect.innerHTML = ""
+  submit.innerHTML = ""
+  
+
   const [level, region, numberRequests] = [e.target.level.value , e.target.region.value, "4"];
   fetchCountries(level, region, numberRequests)
 
@@ -46,6 +71,22 @@ async function fetchCountries(level, region, numberRequests) {
   catch(e){console.log('error at  catch')}
 }
 
+async function fetchImage(type,ID) {
+  
+  //Make sure to add your deployed API URL in this fetch
+   try {
+    const response = await fetch(`http://localhost:3000/image/${type}/${ID}`);
+    if(response.ok){
+      console.log("ok")
+      const mapBolb = await response.blob()
+      const imageURL = URL.createObjectURL(mapBolb)
+      console.log(imageURL)
+      addImage(imageURL)
+    }
+    else {throw 'Error status: ' + response.status}
+  }
+  catch(e){console.log('error at  catch')}
+}
 
 
   
