31f38b5ecd3e496c78eb8a2fc46e96f684d7d9c8
pablo-romeroclavijo <136720541+pablo-romeroclavijo@users.noreply.github.com>
2023-09-19 16:21:43 +0100

    added flag quizz

--- client/flag_quizz/flag_quiz.js
+++ client/flag_quizz/flag_quiz.js
@@ -1,24 +1,47 @@
 const query = document.querySelector("#level")
-const question = document.querySelector('#question')
+const question = document.querySelector('#questions')
 const options = document.querySelector('#options')
 const submitButton = document.querySelector('#answerSubmit')
 const result = document.querySelector('#result')
 
-query.addEventListener("submit", getCountries)
+query.addEventListener("submit", startQuiz)
 question.addEventListener('submit', checkAnwser)
 
+
 let trueAnswer = undefined
+const numberOfQuestions = 10
+let i = 1
+let scores = []
 
 //functions
+
+function startQuiz(e){
+  e.preventDefault()
+  const [level, region, numberRequests] = [e.target.level.value , e.target.region.value, "4"];
+  const numberOfQuestions = 10
+  scores = []
+  i = 1
+
+  getCountries(level, region, numberRequests)
+  }
 
 function createQuestion(data){
   const countries = data
   const randomIndex = Math.floor(Math.random()*countries.length)
   trueAnswer = countries[randomIndex]
 
-  let quest = document.createElement('p')
+  let quest = document.createElement('h3')
+  let number = document.createElement('h2')
+
+  number.classList.add('p-2')
+  quest.classList.add('p-2')
   quest.textContent = `What is the flag of ${trueAnswer.name}?`
-  options.appendChild(quest)
+  number.textContent = `QUESTION ${i} of ${numberOfQuestions}`
+
+  question.appendChild(number)
+  question.appendChild(quest)
+
+
 
   for(country in countries){
     let ID = countries[country].ID
@@ -28,42 +51,50 @@ function createQuestion(data){
   }
   let button = document.createElement('input')
   button.type = `submit`
+  button.classList.add('btn', 'btn-danger', 'position-static', 'm-2')
   submitButton.appendChild(button)
 
 
 }
 
 function addImage(url, ID){
+  let radioBox = document.createElement('div')
+  radioBox.classList.add('radio-box', 'd-inline-flex')
+  options.appendChild(radioBox)
+
+
   let input = document.createElement('input')
+  input.classList.add('radio-input')
   input.type = 'radio'
   input.id = ID
   input.value = ID
   input.name = 'answer'
-  options.appendChild(input)
+  radioBox.appendChild(input)
 
   let label = document.createElement('label')
+  label.classList.add('radio-label')
   label.setAttribute('for', ID)
-  options.appendChild(label)
+  radioBox.appendChild(label)
 
   let img = document.createElement('img')
+  img.classList.add('radio-img')
   img.src = url
   img.width = 150
   label.appendChild(img)
 
 }
 
-function getCountries(e){
-  e.preventDefault()
+function getCountries(level, region, numberRequests){
+  question.innerHTML = ""
   options.innerHTML = ""
   submitButton.innerHTML = ""
   result.innerHTML = ""
   
-
-  const [level, region, numberRequests] = [e.target.level.value , e.target.region.value, "4"];
   fetchCountries(level, region, numberRequests)
 }
 
 function checkAnwser(e){
+  console.log('checking answer')
   e.preventDefault()
   result.innerHTML = ""
   const answer = e.target.answer.value
