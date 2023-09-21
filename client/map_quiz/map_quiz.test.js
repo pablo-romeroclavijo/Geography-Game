5c3b65ebd2d1d99de4a64c94e7a9ffb3ad27f03b
mgr-mirnal <mgr.mrinal97@gmail.com>
2023-09-21 12:13:45 +0100

    test

---
+++ client/map_quiz/map_quiz.test.js
@@ -0,0 +1,11 @@
+giconst each = require('jest-each').default;
+const { checkAnwser} = require("./map_quiz.js");
+
+describe("checkAnswer", () => {
+
+    it("checkAnswer", () => {
+        expect(checkAnwser).toBeDefined();
+    })
+    
+
+});
